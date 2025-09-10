b4bd25c322886e34314f17712b0a2004c0fde78f
Mohammad Nurul Hassan <mdnrlhsn@gmail.com>
2025-09-10 17:20:32 +0200

    employer

--- dbt_code/models/src/src_auxilliary_attributes.sql
+++ dbt_code/models/src/src_auxilliary_attributes.sql
@@ -1,15 +1,6 @@
+with stg_job_ads as (  select *
+    from {{ source('jobtech_analysis', 'stg_ads') }})
 
-
-with stg_job_ads as (
-
-    select *
-    from {{ source('jobtech_analysis', 'stg_ads') }}
-
-)
-
-select
-    row_number() over (order by headline, employer__organization_number) as auxiliary_attributes_id,
-    experience_required,
-    access_to_own_car,
-    driving_license_required
+select 
+     *
 from stg_job_ads
